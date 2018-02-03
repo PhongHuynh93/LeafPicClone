f5efaa3305a81bc9808161ef4e5d4bd7ed51f936
Phong Huỳnh <p7157568@gmail.com>
2018-02-03 09:17:57 +0700

    Splash screen - set color for status bar

--- app/src/main/java/example/test/phong/leafpicclone/SplashScreen.kt
+++ app/src/main/java/example/test/phong/leafpicclone/SplashScreen.kt
@@ -1,7 +1,9 @@
 package example.test.phong.leafpicclone
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 /**
  * Created by user on 2/3/2018.
@@ -10,6 +12,21 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // make screen full screen
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        setNavBarColor()
+        setStatusBarColor()
+    }
+
+    private fun setStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        }
+    }
+
+    private fun setNavBarColor() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
