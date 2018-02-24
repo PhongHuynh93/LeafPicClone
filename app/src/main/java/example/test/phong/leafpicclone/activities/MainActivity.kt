7c962403ddbb4763364f74788c32cfcd95525b72
Phong Huỳnh <p7157568@gmail.com>
2018-02-24 11:55:16 +0700

    Main screen - add listen when select navigation drawer - add theme for left nav

--- app/src/main/java/example/test/phong/leafpicclone/activities/MainActivity.kt
+++ app/src/main/java/example/test/phong/leafpicclone/activities/MainActivity.kt
@@ -1,14 +1,17 @@
 package example.test.phong.leafpicclone.activities
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.databinding.DataBindingUtil
 import android.graphics.Color
 import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v7.app.ActionBarDrawerToggle
 import android.view.View
+import android.widget.Toast
 import com.mikepenz.google_material_typeface_library.GoogleMaterial
 import com.mikepenz.iconics.IconicsDrawable
+import com.orhanobut.hawk.Hawk
 import example.test.phong.leafpicclone.BuildConfig
 import example.test.phong.leafpicclone.R
 import example.test.phong.leafpicclone.activities.base.SharedMediaActivity
@@ -18,11 +21,13 @@ import example.test.phong.leafpicclone.fragments.AlbumClickListener
 import example.test.phong.leafpicclone.fragments.AlbumsFragment
 import example.test.phong.leafpicclone.fragments.EditModeListener
 import example.test.phong.leafpicclone.fragments.NothingToShowListener
+import example.test.phong.leafpicclone.util.Security
 import example.test.phong.leafpicclone.util.addFragment
 import example.test.phong.leafpicclone.util.whenNull
 import example.test.phong.leafpicclone.view.navigation_drawer.NavigationDrawer
 import example.test.phong.leafpicclone.view.themeable.ThemedToolbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_layout.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.debug
 
@@ -55,10 +60,6 @@ class MainActivity : SharedMediaActivity(), AnkoLogger, AlbumClickListener, Edit
         // todo add fragment media
     }
 
-    override fun onPostResume() {
-        super.onPostResume()
-    }
-
     //    fixme should change this name
     override fun changedNothingToShow(isLoading: Boolean) {
         mBinding.isLoading = isLoading
@@ -69,9 +70,12 @@ class MainActivity : SharedMediaActivity(), AnkoLogger, AlbumClickListener, Edit
     }
 
     override fun onAlbumClick(album: Album) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        displayMedia(album)
     }
 
+    private fun displayMedia(album: Album) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     private fun initUi() {
         setSupportActionBar(toolbar as ThemedToolbar)
@@ -95,7 +99,63 @@ class MainActivity : SharedMediaActivity(), AnkoLogger, AlbumClickListener, Edit
         home_navigation_drawer.setAppVersion(BuildConfig.VERSION_NAME)
     }
 
-    override fun onItemSelected(navigationItemSelected: Int) {
+    // // TODO: 2/24/2018 when choosing a item in navigation drawer
+    override fun onItemSelected(@NavigationDrawer.NavigationItem navigationItemSelected: Int) {
+        closeDrawer()
+        when (navigationItemSelected) {
+
+            NavigationDrawer.NAVIGATION_ITEM_ALL_ALBUMS -> displayAlbums(false)
+
+            NavigationDrawer.NAVIGATION_ITEM_ALL_MEDIA -> displayMedia(Album.getAllMediaAlbum())
+
+            NavigationDrawer.NAVIGATION_ITEM_HIDDEN_FOLDERS -> if (Security.isPasswordOnHidden) {
+                askPassword()
+            } else {
+                displayAlbums(true)
+            }
+
+            NavigationDrawer.NAVIGATION_ITEM_WALLPAPERS -> Toast.makeText(this@MainActivity, "Coming Soon!", Toast.LENGTH_SHORT).show()
+
+            NavigationDrawer.NAVIGATION_ITEM_DONATE -> DonateActivity.startActivity(this)
+
+            NavigationDrawer.NAVIGATION_ITEM_SETTINGS -> SettingsActivity.startActivity(this)
+
+            NavigationDrawer.NAVIGATION_ITEM_ABOUT -> AboutActivity.startActivity(this)
+        }
+    }
+
+    private fun askPassword() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun displayAlbums(b: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun closeDrawer() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun updateUiElements() {
+        super.updateUiElements()
+        //TODO: MUST BE FIXED
+        toolbar.popupTheme = popupToolbarStyle
+        toolbar.setBackgroundColor(primaryColor)
+
+        /**** SWIPE TO REFRESH ****/
+
+        setStatusBarColor()
+        setNavBarColor()
+
+        fab_camera.backgroundTintList = ColorStateList.valueOf(accentColor)
+        fab_camera.visibility = if (Hawk.get(getString(R.string.preference_show_fab), false)) View.VISIBLE else View.GONE
+        coordinator_main_layout.setBackgroundColor(backgroundColor)
+
+        setScrollViewColor(home_navigation_drawer)
+
+        home_navigation_drawer.setTheme(primaryColor, backgroundColor, textColor, iconColor)
+
+        // TODO Calvin: This performs a NO-OP. Find out what this is used for
+        setRecentApp(getString(R.string.app_name))
     }
 }
